02b6f68e591a8afd7bee158a48a152389d784699
solitary-1 <1430317507@qq.com>
2020-04-12 16:02:55 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,31 @@
 #include<stdio.h>
 int main()
 {
+    int a[500010];
+    int n;
+    while(1)
+    {
+        int sum=0;
+        scanf("%d",&n);
+        if(n==0) break;
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    sum++;
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        printf("%d\n",sum);
+    }
     return 0;
 }
