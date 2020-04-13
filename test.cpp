ea68150833f04968ec7ea0dcff141c593aa498ec
solitary-1 <1430317507@qq.com>
2020-04-13 22:35:09 +0800

    。

--- test.cpp
+++ test.cpp
@@ -1,72 +1,24 @@
-/*#include<stdio.h>
-int main()
-{
-    int a[500010];
-    int n;
-    while(1)
-    {
-        int sum=0;
-        scanf("%d",&n);
-        if(n==0) break;
-        for(int i=0;i<n;i++)
-        {
-            scanf("%d",&a[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            int flag=0;
-            for(int j=0;j<n-i-1;j++)
-            {
-                if(a[j]>a[j+1])
-                {
-                    flag=1;
-                    sum++;
-                    int temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
-                }
-            }
-            if(flag==0) break;
-        }
-        printf("%d\n",sum);
-    }
-    return 0;
-}*/
-#include<bits/stdc++.h>
+#include<bits\stdc++.h>
 using namespace std;
-const int maxn = 1e5 + 7;
-int viss[maxn];
-int prime[100000+7];
-int flag = 1;
-void Prime(int a)
+void Getnext(int next[],string t)
 {
-    int vis[maxn];
-	memset(vis, 0, sizeof(vis));
-	memset(prime, 0, sizeof(prime));
-    int sum=0;
-    vis[2]=0;
-	for (int i = 2; i <=a+2; i++)
-	{
-		if (!vis[i])
-		{
-            sum++;
-			prime[flag++] = i;
-			for (int j = 2 * i; j <= a+2; j+=i)
-			{
-                if(!vis[j]) vis[j] = sum;
-			}
-		}
-	}
+   int j=0,k=-1;
+   next[0]=-1;
+   while(j<t.length()-1)
+   {
+      if(k == -1 || t[j] == t[k])
+      {
+         j++;k++;
+         next[j] = k;
+      }
+      else k = next[k];//此语句是这段代码最反人类的地方，如果你一下子就能看懂，那么请允许我称呼你一声大神！
+   }
 }
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    Prime(n);
-    for(int i=2;i<n;i++)
-    {
-        printf("%d ",viss[i]);
-    }
-    printf("%d\n",viss[n]);
-    return 0;
+    string t;
+    cin>>t;
+    int next[100];
+    Getnext(next,t);
+    
 }
